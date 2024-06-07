52142abf1c540db290a8d415972dc7916a14edd3
Noble Benjamin <nobleben2008@gmail.com>
2024-06-08 00:30:38 +0100

    Styled the Text div

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@ function App() {
             <img src="/profile-pic/profile.jpg" alt="Profile Card Picture" />
           </div>
           <div id="text">
-            <h2>
-              Noble Benjamin
-            </h2>
+            <h2>Noble Benjamin</h2>
             <p>
-              Hello! I am a skilled MERN stack developer with expertise in HTML, CSS, and JavaScript. I have a passion for creating dynamic and user-friendly web applications that are both visually appealing and functional. With years of experience in front-end and back-end development, I am confident in my ability to tackle any project and deliver high-quality results
+              Hello! I am a skilled MERN stack developer with expertise in HTML,
+              CSS, and JavaScript. I have a passion for creating dynamic and
+              user-friendly web applications that are both visually appealing
+              and functional. With years of experience in front-end and back-end
+              development, I am confident in my ability to tackle any project
+              and deliver high-quality results
             </p>
           </div>
           <div id="skills"></div>
