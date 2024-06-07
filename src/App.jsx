7bb1d23671d83e3b4f48767303c48a2017692fd4
Noble Benjamin <nobleben2008@gmail.com>
2024-06-08 00:29:11 +0100

    Added content for profile Card

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
             <h2>
               Noble Benjamin
             </h2>
+            <p>
+              Hello! I am a skilled MERN stack developer with expertise in HTML, CSS, and JavaScript. I have a passion for creating dynamic and user-friendly web applications that are both visually appealing and functional. With years of experience in front-end and back-end development, I am confident in my ability to tackle any project and deliver high-quality results
+            </p>
           </div>
           <div id="skills"></div>
         </div>
