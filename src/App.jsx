62d466613f10d8d2ecba2e8833a772bef2bd1cf7
Noble Benjamin <nobleben2008@gmail.com>
2024-06-08 00:28:39 +0100

    Added name for profile Card

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ function App() {
           <div id="picture">
             <img src="/profile-pic/profile.jpg" alt="Profile Card Picture" />
           </div>
-          <div id="text"></div>
+          <div id="text">
+            <h2>
+              Noble Benjamin
+            </h2>
+          </div>
           <div id="skills"></div>
         </div>
       </div>
