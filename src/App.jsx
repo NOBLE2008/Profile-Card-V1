8d1aa618c7471c12e8d0676c4602053d8c5c7f83
Noble Benjamin <nobleben2008@gmail.com>
2024-06-07 23:46:01 +0100

    Added divs for profile picture, text and skills

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ function App() {
   return <>
     <div id="container">
       <div id="card">
-  
+        <div id="picture">
+
+        </div>
+        <div id="text">
+
+        </div>
+        <div id="skills">
+
+        </div>
       </div>
     </div>
   </>;
