4372709ff976cc13c33f3a15dfb2c9f2272b141b
Noble Benjamin <nobleben2008@gmail.com>
2024-06-08 00:05:43 +0100

    Styled the picture div

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import "./App.css";
 
 function App() {
-
-  return <>
-    <div id="container">
-      <div id="card">
-        <div id="picture">
-
-        </div>
-        <div id="text">
-
-        </div>
-        <div id="skills">
-
+  return (
+    <>
+      <div id="container">
+        <div id="card">
+          <div id="picture">
+            <img src="/profile-pic/profile.jpg" alt="Profile Card Picture" />
+          </div>
+          <div id="text"></div>
+          <div id="skills"></div>
         </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 }
 
 export default App;
